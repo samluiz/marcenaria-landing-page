629fd1774a3d8ad75425ed3355b39ee390d9131d
Samuel Luiz <samuel.luiz@datacake.com.br>
2022-10-05 20:02:37 -0300

    making it SEO friendly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gilbertoApp';
+
+  constructor(private metaTagService: Meta) {}
+
+  ngOnInit(): void {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content:
+          'Serralheiro, Serralheria, Soldador, Soldar, Serviço, Trabalhador, Gilberto, Gilberto Machado, SAURS, Samuel Luiz, Grade, Portão',
+      },
+      {
+        name: 'description',
+        content:
+          'Serralheiro Gilberto Machado, Parnaíba - PI. Forjando soluções nos mínimos detalhes',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Samuel Luiz' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: 'UTF-8 ' },
+    ]);
+  }
 }
